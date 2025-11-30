3eff30449ea1e50c40d9b746a4493d839ea06119
Aditya Shejwal <adityashejwal016@gmail.com>
2025-11-30 13:17:17 +0530

    Add files via upload

---
+++ Program54.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int CheckPrime(int iNo)
+{
+
+  int iCnt=0, iFrequency=0;
+
+  if(iNo<0)
+ {
+    {
+        iNo= -iNo;
+    }
+    
+    for(iCnt=2; iCnt<= (iNo / 2); iCnt++)
+    {
+        if((iNo % iCnt )== 0)
+        {
+            iFrequency++;
+        }
+    }
+     
+ }
+    if(iFrequency==0)             // no factor
+    {
+        return true;
+    }
+    else
+    {
+        return false;            // Atleast 1 factor
+    }
+}
+
+int main()
+{
+    int iValue=0;
+    bool bRet = false;
+
+    printf("Enter the number :\n");
+    scanf("%d",&iValue);
+
+    bRet=CheckPrime(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is prime number\n", iValue);
+
+    }
+    else
+    {
+         printf("%d is not a prime number\n", iValue);
+
+    }
+
+    
+    return 0;
+}
---
+++ Program59.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int CheckPrime(int iNo)
+{
+
+  int iCnt=0;
+  bool bFlag= false;
+
+
+  if(iNo<0)
+ 
+    {
+        iNo= -iNo;
+    }
+
+    for(iCnt=2, bFlag=true; iCnt<= (iNo / 2); iCnt++)
+    {
+        if((iNo % iCnt )== 0)
+        {
+            bFlag=false;
+            break;                    //Otimizatiojn
+        }
+    }
+    
+    return bFlage;
+
+}
+ 
+
+int main()
+{
+    int iValue=0;
+    bool bRet = false;
+
+    printf("Enter the number :\n");
+    scanf("%d",&iValue);
+
+    bRet=CheckPrime(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is prime number\n", iValue);
+
+    }
+    else
+    {
+         printf("%d is not a prime number\n", iValue);
+
+    }
+
+    
+    return 0;
+}
+
+
+// time complexcitie " for prime - N/2"
+// time complexcity : for non prime - eithe 1 or 0
