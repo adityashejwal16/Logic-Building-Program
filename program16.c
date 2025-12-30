7ca6145caf8a5f57ceb52b4b3ee1ba84e67d55d5
Aditya Shejwal <adityashejwal016@gmail.com>
2025-12-30 10:08:48 +0530

    Add files via upload

--- program16.c
+++ program16.c
@@ -1,110 +1,54 @@
-///////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 //
+// Required Header files
 //
-//  Required Header file
-//
-////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
-#include<stdio.h>                     // For Input output
-#include<stdbool.h>                   // for bool datatype
-//////////////////////////////////////////////////////////////////////////
+#include<stdio.h>               // For Input Output
+#include<stdbool.h>             // For bool datatype
+
+/////////////////////////////////////////////////////////////
 //
-// Function Name : CheckEvenOdd
-// Description :   it is used to check even or odd
-// Input :         Integer
-// Output :        Boolean
-// Author :        Aditya Shejwal
-// Date :          10/10/2025
+//  Function Name :     CheckEvenOdd
+//  Description :       It is used to check even or odd
+//  Input :             Integer
+//  Output :            Boolean
+//  Author :            Aditya Shejwal
+//  Date :              10/10/2025
 //
-//
-/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 
 bool CheckEvenOdd(int iNo)
 {
-    int iRem=0;
-
+    int iRem = 0;
     iRem = iNo % 2;
+
     if(iRem == 0)
-    { return true; }
+    {   return true;    }
     else
-    { return false;}
-
+    {   return false;   }
 }
 
-/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
 //
-// Entry #include<stdio.h>
-#include<stdbool.h>
-
-bool CheckEvenOdd(int iNo)
-{
-    return (( iNo % 2)==0);
-}
+//  Entry point function of the application
+//  
+/////////////////////////////////////////////////////////////
 
 int main()
 {
-    int iValue=0;
-    bool bRet=false;
+    int iValue = 0;
+    bool bRet = false;
 
-    printf("Enter number :");
-    scanf("%d", &iValue);
+    printf("Enter number : ");
+    scanf("%d",&iValue);
 
-  bRet = CheckEvenOdd(iValue);
-  
-  if(bRet==true)
-  {
-    printf("%d is  Even number\n",iValue);
-  }
-  else
-  {
-    printf("%d is  odd number\n",iValue);
-  }
-    return 0;
-}#include<stdio.h>
-#include<stdbool.h>
+    bRet = CheckEvenOdd(iValue);
 
-bool CheckEvenOdd(int iNo)
-{
-    return (( iNo % 2)==0);
-}
+    if(bRet == true)
+    {   printf("%d is Even number\n",iValue);   }
+    else
+    {   printf("%d is Odd number\n",iValue);    }
 
-int main()
-{
-    int iValue=0;
-    bool bRet=false;
-
-    printf("Enter number :");
-    scanf("%d", &iValue);
-
-  bRet = CheckEvenOdd(iValue);
-  
-  if(bRet==true)
-  {
-    printf("%d is  Even number\n",iValue);
-  }
-  else
-  {
-    printf("%d is  odd number\n",iValue);
-  }
-    return 0;
-}
-//
-//
-//////////////////////////////////////////////////////
-
-int main()
-{
-    int iValue=0;
-    bool bRet=false;
-
-    printf("Enter number :");
-    scanf("%d", &iValue);
-
-  bRet = CheckEvenOdd(iValue);
-  
-  if(bRet==true)
-  {  printf("%d is  Even number\n",iValue); }
-  else
-  {  printf("%d is  odd number\n",iValue);  }
     return 0;
 }
