dfb94a79ed8b410e61d74e68ab533058f384a549
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-06 23:37:18 +0530

    Add files via upload

---
+++ program336.js
@@ -0,0 +1,18 @@
+function Addition(iNo1,iNo2)
+{
+   let iAns = 0;
+   iAns = iNo1 + iNo2;
+   return iAns;
+}
+function main()
+{
+      let iValue =10, iValue2=11;
+      let iRet =0;
+
+      iRet = Addition(iValue,iValue2);
+
+      console.log("Addition is :" ,iRet);
+
+}
+
+main();  //OS
