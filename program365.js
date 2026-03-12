9941559c4f50a39e76dbd9c52d872c298a1013c1
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-12 23:52:51 +0530

    Add files via upload

---
+++ program365.js
@@ -0,0 +1,15 @@
+
+function main()
+{
+  let obj = {name : "Aditya", salary : 21000};    // JSON
+
+ 
+
+  // console.log(obj[0]);   NOT ALLOWED 
+
+  console.log(obj.name);
+  console.log(obj.salary);
+
+   // console.log(obj(name));   NOT ALLOWED
+}
+main();
