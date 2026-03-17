7db532d36eba1f600fe3bae9a19b45eea933c424
Aditya Shejwal <adityashejwal016@gmail.com>
2026-03-17 22:09:16 +0530

    Add files via upload

---
+++ program382.js
@@ -0,0 +1,21 @@
+function count(str)
+{
+let iCount =0;
+
+  for(iCnt=0; iCnt<str.length; iCnt++)
+  {
+    if(str.charAt(iCnt) == 'l');
+    {
+       iCount ++;
+    }
+  }
+  return iCount;
+}
+function main()
+{
+  let data = "Hello";
+  let iCnt =0;
+  
+ 
+}
+main();
