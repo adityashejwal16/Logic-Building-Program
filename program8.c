7a5e5f01c477d10e7923bc51fd69a30864edfaee
Aditya Shejwal <adityashejwal016@gmail.com>
2026-04-25 23:51:38 +0530

    Add files via upload

--- program8.c
+++ program8.c
@@ -1,50 +1,45 @@
-/* 
-  step 1 : understand the problem statement
-  step 1 : write the algoritham
-  step 3 : Decide the programming language
-  step 4 : write the program
-  step 5 : test the program
-
 /*
-       ALGORITHAM
-   START
-
-   acept firts number no1
-   acept secound number no1
-   perform Addition off no1&no2
-   Display the Addition on Screen
-
-   STOP
-
-
+    Step 1 : Understand the problem statement
+    Step 2 : Write the algorithm
+    Step 3 : Decide the programming language
+    Step 4 : Write the program
+    Step 5 : Test the program
 */
 
 
-#include<stdio.h>
+/*
+    Algorithm
 
+    START
+        Accept first number as no1
+        Accept second number as no2
+        Perform Addition of no1 & no2
+        Display the addition on screen
+    STOP
+*/
+
+#include<stdio.h>
 
 float AdditionTwoNumbers(float fNo1, float fNo2)
 {
-    float fsum = 0.0f;
-    fsum = fNo1 + fNo2;          //business logic
-    return fsum;
+    float fSum = 0.0f;
+    fSum = fNo1 + fNo2;    // Business Logic
+    return fSum;
 }
 
 int main()
 {
-    float fValue1=0, fValue2= 0, fRet =0;
+    float fValue1 = 0.0f, fValue2 = 0.0f, fRet = 0.0f;
 
-    printf("Enter first number:\n");
+    printf("Enter first number : \n");
     scanf("%f",&fValue1);
 
-    printf("Enter secound number:\n");
+    printf("Enter second number : \n");
     scanf("%f",&fValue2);
+    
+    fRet = AdditionTwoNumbers(fValue1,fValue2);
 
-
-    fRet=AdditionTwoNumbers(fValue1,fValue2);
-
-
-    printf("Addition is: %f\n",fRet);
+    printf("Addition is : %f\n",fRet);
 
     return 0;
 }
